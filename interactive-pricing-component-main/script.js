adf351da1897c67d0a7cc164357dec5c0936bfb1
Raymond-15 <raymondputra15@gmail.com>
2021-08-21 00:10:58 +0800

    almost done

---
+++ interactive-pricing-component-main/script.js
@@ -0,0 +1,47 @@
+var slider = document.getElementById("range");
+var output = document.getElementsByClassName("price-permonth")[0];
+var output1 = document.getElementsByClassName("price-permonth")[1];
+var pages = document.getElementsByClassName("pages-permonth")[0];
+var checkbox = document.getElementsByClassName("toggle")[0];
+var year = document.getElementsByClassName("month")[0];
+output.innerHTML = "$" + slider.value + ".00";
+output1.innerHTML = "$" + slider.value + ".00";
+pages.innerHTML = slider.value * 6.25 + " K";
+var descuesnto = 0;
+var rango = 16;
+
+slider.style.background = "linear-gradient(to right, hsl(174, 77%, 80%) " + (slider.value / 32) * 100 + "%, hsl(224, 65%, 95%) " + (slider.value / 32) * 100 + "%)";
+
+checkbox.addEventListener("change", function () {
+  if (this.checked) {
+    descuesnto = 1;
+    year.innerHTML = " /year";
+    output.innerHTML = "$ " + (slider.value * 12 - slider.value * 12 * 0.25).toFixed(2);
+    output1.innerHTML = "$ " + (slider.value * 12 - slider.value * 12 * 0.25).toFixed(2);
+  } else {
+    year.innerHTML = "  /month";
+    descuesnto = 0;
+    output.innerHTML = "$ " + ((slider.value / 32) * 32).toFixed(2);
+    output1.innerHTML = "$ " + ((slider.value / 32) * 32).toFixed(2);
+  }
+});
+
+slider.oninput = function () {
+  if (descuesnto == 1) {
+    output.innerHTML = "$ " + (this.value * 12 - this.value * 12 * 0.25).toFixed(2);
+    rango = output.innerHTML;
+    output1.innerHTML = "$ " + (this.value * 12 - this.value * 12 * 0.25).toFixed(2);
+    rango = output1.innerHTML;
+  } else {
+    output.innerHTML = "$ " + ((this.value / 32) * 32).toFixed(2);
+    rango = output.innerHTML;
+    output1.innerHTML = "$ " + ((this.value / 32) * 32).toFixed(2);
+    rango = output1.innerHTML;
+  }
+  if (slider.value > 16) {
+    pages.innerHTML = parseInt(slider.value * 15.63) + " K";
+  } else {
+    pages.innerHTML = parseInt(slider.value * 6.25) + " K";
+  }
+  slider.style.background = "linear-gradient(to right, hsl(174, 77%, 80%) " + (slider.value / 32) * 100 + "%, hsl(224, 65%, 95%) " + (slider.value / 32) * 100 + "%)";
+};
